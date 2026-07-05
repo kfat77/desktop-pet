f2164c1fcf63f2e602860e0c08226160f03a1ef3
kfat77 <kfat750@gmail.com>
2026-07-05 21:36:15 +0800

    fix: 修复 tray_app.py 中文引号导致的 SyntaxError
    将 QMessageBox 的 HTML 字符串改用 Unicode 转义（\U0001F431、\u2753 等），
    避免中文引号/emoji 与 Python 字符串定界符冲突。

--- desktop_pet/tray_app.py
+++ desktop_pet/tray_app.py
@@ -23,23 +23,23 @@ class TrayApp(QObject):
         # 创建托盘
         self._tray = QSystemTrayIcon(self)
         self._tray.setIcon(self._icon)
-        self._tray.setToolTip("桌面宠物 🐱 - 左右键拖动召唤小猫")
+        self._tray.setToolTip("桌面宠物 \U0001F431 - 左右键拖动召唤小猫")
         
         # 创建菜单
         self._menu = QMenu()
         
         # 添加菜单项
-        self._show_info = self._menu.addAction("🐱 桌面宠物已运行")
+        self._show_info = self._menu.addAction("\U0001F431 桌面宠物已运行")
         self._show_info.setEnabled(False)
         
         self._menu.addSeparator()
         
-        self._show_help = self._menu.addAction("❓ 使用说明")
+        self._show_help = self._menu.addAction("\u2753 使用说明")
         self._show_help.triggered.connect(self._on_help)
         
         self._menu.addSeparator()
         
-        self._exit_action = self._menu.addAction("🚪 退出")
+        self._exit_action = self._menu.addAction("\U0001F6AA 退出")
         self._exit_action.triggered.connect(self._on_exit)
         
         self._tray.setContextMenu(self._menu)
@@ -100,24 +100,23 @@ class TrayApp(QObject):
     
     def _on_help(self):
         """显示帮助"""
-        QMessageBox.information(
-            None,
-            "桌面宠物 - 使用说明",
-            "<h2>🐱 桌面宠物</h2>"
-            "<p><b>召唤小猫：</b></p>"
-            "<ol>"
-            "<li>按住鼠标<b>左键 + 右键</b>同时不放</li>"
-            "<li>向任意方向拖动，画出矩形框</li>"
-            "<li>松开鼠标，小猫会从框中探出脑袋，然后跳出来！</li>"
-            "</ol>"
-            "<p><b>互动：</b></p>"
-            "<ul>"
-            "<li>点击小猫：它会跳跃并叫"喵~"</li>"
-            "<li>拖动小猫：可以把它放到任意位置</li>"
-            "<li>小猫会自动在桌面上走动</li>"
-            "</ul>"
-            "<p><b>退出：</b>右键点击托盘图标 → 退出</p>"
+        help_text = (
+            '<h2>\U0001F431 桌面宠物</h2>'
+            '<p><b>召唤小猫：</b></p>'
+            '<ol>'
+            '<li>按住鼠标<b>左键 + 右键</b>同时不放</li>'
+            '<li>向任意方向拖动，画出矩形框</li>'
+            '<li>松开鼠标，小猫会从框中探出脑袋，然后跳出来！</li>'
+            '</ol>'
+            '<p><b>互动：</b></p>'
+            '<ul>'
+            '<li>点击小猫：它会跳跃并叫&#12300;喵~&#12301;</li>'
+            '<li>拖动小猫：可以把它放到任意位置</li>'
+            '<li>小猫会自动在桌面上走动</li>'
+            '</ul>'
+            '<p><b>退出：</b>右键点击托盘图标 \u2192 退出</p>'
         )
+        QMessageBox.information(None, "桌面宠物 - 使用说明", help_text)
     
     def _on_exit(self):
         """退出程序"""
